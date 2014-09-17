b4161d396a9edead4f9c321afca7acd82a42e666
Arba Sasmoyo <asasmoyo@outlook.com>
2014-09-17 21:41:44 +0700

    add SMTPEmail dan EMailLogger

---
+++ app/protected/libraries/logger/EMailLogger.php
@@ -0,0 +1,37 @@
+<?php
+
+class EMailLogger extends CEmailLogRoute
+{
+
+    public $smtpServer;
+    public $smtpUsername;
+    public $smtpPassword;
+    public $smtpPort;
+    public $loggedUserAttributes = array();
+
+    protected function sendEmail($email, $subject, $message)
+    {
+        $data = array();
+        foreach ($this->loggedUserAttributes as $attribute) {
+            $data[$attribute] = Yii::app()->user->$attribute;
+        }
+
+        $detailStr = "\r\n--- REQUEST DETAILS ---\r\n" . json_encode($data);
+
+        $sendEmail = new SMTPMail(
+            $this->smtpServer,
+            $this->smtpUsername,
+            $this->smtpPassword,
+            $this->smtpPort
+        );
+
+        $sendEmail->sendPlainEmail(
+            $this->smtpUsername,
+            $this->smtpUsername,
+            $email,
+            $subject,
+            $message . $detailStr
+        );
+    }
+
+} 
